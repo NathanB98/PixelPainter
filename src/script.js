6ec8e12c207915f712f861736799dc1a7a742578
NathanB98 <97767277+NathanB98@users.noreply.github.com>
2023-07-09 20:54:36 +0100

    Canvas grid can now be resized using a slider, size spans 1x1 to 64x64 inclusive.

--- src/script.js
+++ src/script.js
@@ -1,11 +1,24 @@
 const GRID_CONTAINER = document.querySelector('.grid-container');
 const CLEAR_BUTTON = document.querySelector('.clear-btn');
+const GRID_SIZE_SLIDER = document.querySelector('.size-slider');
+const GRID_SIZE_DISPLAY = document.querySelector('.size-display');
 
 let gridLength = 16;
 let gridVolume = gridLength * gridLength;
 
 CLEAR_BUTTON.onclick = () => clearGrid();
 
+/* Changes the size of the canvas grid to a size given by the user via a slider element. Resizing clears the grid.
+Minimum size: 1x1, Maximum size: 64x64. */
+GRID_SIZE_SLIDER.oninput = function() {
+    GRID_SIZE_DISPLAY.innerHTML = `${this.value} x ${this.value}`;
+    gridLength = this.value;
+    gridVolume = gridLength * gridLength;
+    clearGrid();
+}
+
+/* Removes all of the elements that make up the grid from the DOM, wiping the canvas of any drawings, then loads a new grid using 
+the generateCanvasGrid function. */
 function clearGrid() {
     let gridSquare = document.querySelectorAll('.grid-square');
     gridSquare.forEach(pixel => {
@@ -14,6 +27,8 @@ function clearGrid() {
     generateCanvasGrid(gridVolume);
 }
 
+/* Generates a square grid of a given length through DOM manipulation. Gives each element (or pixel) in the grid an eventListener. 
+Takes the gridLength which will define the size of the grid generated.*/
 function generateCanvasGrid(volume) {
     GRID_CONTAINER.style.gridTemplateColumns = `repeat(${gridLength}, 1fr)`;
     GRID_CONTAINER.style.gridTemplateRows = `repeat(${gridLength}, 1fr)`;

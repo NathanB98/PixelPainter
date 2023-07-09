09f15c30d61ef2edfccc04e545d5e4a9579bd8d4
NathanB98 <97767277+NathanB98@users.noreply.github.com>
2023-07-09 21:23:12 +0100

    Brush, rainbow, and eraser modes implemented, now user can draw in either black, a random colour, or erase a specific pixel from the canvas.

--- src/script.js
+++ src/script.js
@@ -2,11 +2,17 @@ const GRID_CONTAINER = document.querySelector('.grid-container');
 const CLEAR_BUTTON = document.querySelector('.clear-btn');
 const GRID_SIZE_SLIDER = document.querySelector('.size-slider');
 const GRID_SIZE_DISPLAY = document.querySelector('.size-display');
+const BRUSH_BUTTON = document.querySelector('.brush-btn');
+const RAINBOW_BUTTON = document.querySelector('.rainbow-btn');
+const ERASER_BUTTON = document.querySelector('.eraser-btn');
 
 let gridLength = 16;
 let gridVolume = gridLength * gridLength;
+let currentMode = 'brush';
 
-CLEAR_BUTTON.onclick = () => clearGrid();
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
 
 /* Changes the size of the canvas grid to a size given by the user via a slider element. Resizing clears the grid.
 Minimum size: 1x1, Maximum size: 64x64. */
@@ -15,6 +21,39 @@ GRID_SIZE_SLIDER.oninput = function() {
     gridLength = this.value;
     gridVolume = gridLength * gridLength;
     clearGrid();
+}
+CLEAR_BUTTON.onclick = () => clearGrid();
+BRUSH_BUTTON.onclick = () => setPaintMode('brush');
+RAINBOW_BUTTON.onclick = () => setPaintMode('rainbow');
+ERASER_BUTTON.onclick = () => setPaintMode('eraser');
+
+/* Paints the appropriate brush type to the canvas depending on the users choice of mode. Either brush, rainbow, or an eraser. */
+function changeBrushColour(e) {
+    if(e.type === 'mouseover' && !mouseDown) return;
+    if(currentMode === 'rainbow') {
+        const randomR = Math.floor(Math.random() * 256);
+        const randomG = Math.floor(Math.random() * 256);
+        const randomB = Math.floor(Math.random() * 256);
+        e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`
+    } else if(currentMode === 'brush') {
+        e.target.style.backgroundColor = 'black';
+    } else if(currentMode === 'eraser') {
+        e.target.style.backgroundColor = '#FFFFFF';
+    }
+}
+
+function setPaintMode(mode) {
+    switch(mode) {
+        case 'brush':
+            currentMode = 'brush';
+            break;
+        case 'rainbow':
+            currentMode = 'rainbow';
+            break;
+        case 'eraser':
+            currentMode = 'eraser';
+            break;
+    }
 }
 
 /* Removes all of the elements that make up the grid from the DOM, wiping the canvas of any drawings, then loads a new grid using 
@@ -37,9 +76,8 @@ function generateCanvasGrid(volume) {
         let gridSquare = document.createElement('div');
 
         gridSquare.classList.add('grid-square');
-        gridSquare.addEventListener('mouseover', () => {
-            gridSquare.style.backgroundColor = 'black';
-        });
+        gridSquare.addEventListener('mousedown', changeBrushColour);
+        gridSquare.addEventListener('mouseover', changeBrushColour);
         GRID_CONTAINER.appendChild(gridSquare);
     }
 }

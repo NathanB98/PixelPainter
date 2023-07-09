d8c5fa3cba80ae5b14edbf60e832540b71540890
NathanB98 <97767277+NathanB98@users.noreply.github.com>
2023-07-09 19:50:41 +0100

    minor changes to coding conventions for easier readability.

--- src/script.js
+++ src/script.js
@@ -1,17 +1,17 @@
-const gridContainer = document.querySelector('.grid-container');
+const GRID_CONTAINER = document.querySelector('.grid-container');
 
 let gridLength = 16;
 let gridVolume = gridLength * gridLength;
 
 function generateCanvasGrid(volume) {
-    gridContainer.style.gridTemplateColumns = `repeat(${gridLength}, 1fr)`;
-    gridContainer.style.gridTemplateRows = `repeat(${gridLength}, 1fr)`;
+    GRID_CONTAINER.style.gridTemplateColumns = `repeat(${gridLength}, 1fr)`;
+    GRID_CONTAINER.style.gridTemplateRows = `repeat(${gridLength}, 1fr)`;
 
     for(let i = 0; i < volume; i++) {
         let gridSquare = document.createElement('div');
 
         gridSquare.classList.add('grid-square');
-        gridContainer.appendChild(gridSquare);
+        GRID_CONTAINER.appendChild(gridSquare);
     }
 }
 
